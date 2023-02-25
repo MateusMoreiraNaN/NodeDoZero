b96583b5d478749b4903a2131f6e12b1c2d6b3ae
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-02-25 16:54:01 -0300

    subir

---
+++ Seção2Fundamentos/src/1ModuloInterno/index.ts
@@ -0,0 +1,5 @@
+import meuModulo from "./meuModulo"
+
+const somar = meuModulo.soma
+
+somar(2, 3)
---
+++ Seção2Fundamentos/src/1ModuloInterno/meuModulo.ts
@@ -0,0 +1,16 @@
+/*
+modulo.exports = {
+    soma(a,b){
+        console.log(a+b);
+        
+    }
+}
+*/
+
+function soma(a:number, b:number): number{
+    return a + b
+}
+
+export default{
+    soma
+}
